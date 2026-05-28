2542c602b2d7600f58fb5413eb11e69abd9e6984
ClarrenceAdriano <cadriano@student.ciputra.ac.id>
2026-05-28 21:57:15 +0700

    Initial Commit

---
+++ FillIn/FillInApp.swift
@@ -0,0 +1,17 @@
+//
+//  FillInApp.swift
+//  FillIn
+//
+//  Created by Clarrence Adriano Hemeldan on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct FillInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
