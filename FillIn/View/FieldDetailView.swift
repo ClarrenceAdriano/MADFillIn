1909d38539ffb565cfd2a3ac5541efb69c8667c0
ShatryaChristiano <schristiano@student.ciputra.ac.id>
2026-05-29 09:55:04 +0700

    Field ViewModel and View

---
+++ FillIn/View/FieldDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FieldDetailView.swift
+//  FillIn
+//
+//  Created by Shatrya Christiano on 29/05/26.
+//
+
+import SwiftUI
+
+struct FieldDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FieldDetailView()
+}
---
+++ FillIn/ViewModel/FieldViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FieldViewModel.swift
+//  FillIn
+//
+//  Created by Shatrya Christiano on 29/05/26.
+//
+
+import Foundation
